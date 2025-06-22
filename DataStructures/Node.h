1dfd431f92168d4c4dbe0fff713321c7a38e77b7
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2025-06-22 16:23:57 +0600

    feature: try to make it more type agnostic

--- DataStructures/Node.h
+++ DataStructures/Node.h
@@ -2,10 +2,26 @@
 #define Node_h
 
 
-struct Node_int{
 
-    int data;
-    struct Node_int* next;
+enum DataType {
+    INT,
+    FLOAT,
+    DOUBLE,
+    CHAR,
+    Bool,
+    STRING
 };
+
+
+struct Node{
+
+    void *data;
+    int data_type; 
+    int size;
+    struct Node* next;
+};
+
+struct Node* node_Constructor(void* data, int data_type, int size);
+void node_Destructor(struct Node* node);
 
 #endif
